d25f9d4ed6a5a477f48cfa03255907697aa8f177
Laurie Yeh <laurie8925@gmail.com>
2024-04-20 01:03:02 -0700

    added about section mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,16 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className='about-section flex flex-col items-center justify-center h-screen bg-gray-300 overflow-hidden'>
-      About
+    <div className='about-section flex flex-col items-center justify-center h-screen overflow-hidden'>
+      <p className='font-semibold text-pretty'>Hello &#128075; Welcome to my corner of the web! <br/>
+      My name is Laurie, and I’m a Front End Web Developer and UX Designer. </p>
+
+      <br/>
+
+     <p> I recently graduated from BCIT’s Front End Web Development Program after I finished my Bachelor of Science Degree from University of Toronto. </p> <br/>
+
+      <p>I enjoy the idea of being capable of turning user-friendly webpages and aesthetic designs into reality. I am fascinated by the interaction between technology and creativity. The process of bringing concepts and ideas to life, shaping them into interactive and visually appealing websites, motivates me to continue doing what I love. </p> <br/>
+      <p> Aside from coding, I enjoy going on walks, trying out local food and dessert, on a journey to find all the good Matcha place, and look at cat memes. </p> 
     </div>
   );
 }
