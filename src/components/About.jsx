5c209d7728f6c827b9983f4799872092b7146af8
Laurie Yeh <laurie8925@gmail.com>
2024-05-03 01:16:43 -0700

    added skills and loading screen

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,41 +8,42 @@ import 'aos/dist/aos.css';
 
 const About = () => {
     useEffect(() => {
-        AOS.init({
-            duration: 800, // Set the duration of AOS animations to 800ms
+        AOS.init({ 
+            duration: 1000, 
         });
     }, []);
 
     return (
-        <section className='about flex flex-col items-center justify-center h-screen overflow-hidden' id="about"
-       >
+        <section className='about flex flex-col items-center justify-center  overflow-hidden pt-60' id="about">
             <InView as="div"oonChange={(inView, entry) => {}}>
                 {InView ? (
-                     <article className='max-w-sm md:max-w-5xl text-pretty md:text-lg lg:text-xl' >
+                <article className='max-w-sm md:max-w-5xl text-pretty md:text-lg lg:text-xl' >
                 
 
-                <p className='font-semibold' data-aos="fade-up"  data-aos-duration="20000">
+                <p className='font-semibold' data-aos="fade-up"  >
                     Hello 👋 Welcome to my corner of the web! <br/>
                     My name is Laurie, and I’m a Front End Web Developer and UX Designer.
                 </p>
 
                 <br/>
 
-                <p data-aos="fade-up"  data-aos-duration="20000">
+                <p data-aos="fade-up"  >
                     I recently graduated from BCIT’s Front End Web Development Program after I finished my Bachelor of Science Degree from University of Toronto.
                 </p>
 
                 <br/>
 
-                <p data-aos="fade-up"  data-aos-duration="20000">
+                <p data-aos="fade-up"  >
                     I enjoy the idea of being capable of turning user-friendly webpages and aesthetic designs into reality. I am fascinated by the interaction between technology and creativity. The process of bringing concepts and ideas to life, shaping them into interactive and visually appealing websites, motivates me to continue doing what I love.
                 </p>
 
                 <br/>
 
-                <p data-aos="fade-up"  data-aos-duration="20000">
-                    Aside from coding, I enjoy going on walks, trying out local food and dessert, on a journey to find all the good Matcha place, and look at cat memes.
+                <p data-aos="fade-up"  >
+                    Aside from coding, I enjoy going on walks, trying out local food and dessert 🍰, on a journey to find all the good Matcha place 🍵, and look at cat memes😹.
                 </p>
+
+                <Skills />
             </article>
             
                 ) : ""}
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,22 +1,93 @@
 import {useState, useEffect} from 'react'
 
 import skills from "../global/techstack.json"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export const Skills = () => {
 
     const[skilslData, setSkillskData] = useState(null); 
+    const [showing, setShowing] = useState(false)
+    const onClick = () => (showing) ? setShowing(false) : setShowing(true)
+    
 
     useEffect(() => { 
         setSkillskData(skills);       
     },[]); 
 
+    useEffect(() => {
+        AOS.init({
+            duration: 1000, 
+        });
+    }, []);
+
   return (
-    <div>Skills
-        {skills.map((skills, index) => (
-            <div key={index}>
-                <p>{skills.title}</p>
+    <div className='pt-10'>
+    <div data-aos="fade-up" >
+       
+            {showing ?
+            <div>
+             <button onClick={onClick} className='plus-icon flex items-center justify-center pb-5 cross'>
+            
+             <h3 className='font-semibold text-theme' >Click to look at some of the Tools I use!</h3>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus pl-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                 <path d="M12 5l0 14" className='stroke-theme'/>
+                 <path d="M5 12l14 0" className='stroke-theme'/>
+                 </svg>
+             </button>
+             </div>
+             : 
+             <button onClick={onClick} className='plus-icon flex items-center justify-center pb-5 plus'>
+            
+             <h3 className='font-semibold' >Click to look at some of the Tools I use!</h3>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus pl-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                 <path d="M12 5l0 14" />
+                 <path d="M5 12l14 0" />
+                 </svg>
+             </button>
+             }
+            
+        
+       
+        { showing ? 
+        <div data-aos="fade-down ">
+            {/* Development Skills */}
+            <div className='w-full pb-5' >
+                <h3>Development</h3>
+                <div className='flex flex-wrap flex-row gap-2 pt-2 w-full'>
+                    {skills.map((skill, index) => (
+                        skill.type === "dev" && (
+                            <div key={index} className='flex flex-wrap flex-row items-center w-full h-auto skill-card'>
+                                <img src={`/src/medias/logos/${skill.id}-logo.png`} alt={skill.title} className='w-8 h-8 md:w-16 md:h-16 block object-contain p-2'/>
+                                <p className='text-sm'>{skill.title}</p>
+                            </div>
+                        )
+                    ))}
+                </div>
             </div>
-        ))}
+
+            {/* Design Skills */}
+            <div className='w-full'  >
+                <h3>Design</h3>
+                <div className='flex flex-wrap flex-row gap-5 pt-2 w-full'>
+                    {skills.map((skill, index) => (
+                        skill.type === "design" && (
+                            <div key={index} className='flex flex-wrap flex-row items-center w-full h-auto skill-card'>
+                                <img src={`/src/medias/logos/${skill.id}-logo.png`} alt={skill.title} className='w-8 h-8 md:w-16 md:h-16 block object-contain p-2'/>
+                                <p className='text-sm'>{skill.title}</p>
+                            </div>
+                        )
+                    ))}
+                </div>
+            </div>
+        </div>
+        : null
+        }
+     
     </div>
-  )
+</div>
+
+    )
 }
